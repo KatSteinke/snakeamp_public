00da625ae787e716878838cc3432ad603dd0dad6
Kat Steinke <kat.steinke@rsyd.dk>
2023-09-06 14:34:24 +0200

    pipeline_config: fix path

--- pipeline_config.py
+++ pipeline_config.py
@@ -6,7 +6,7 @@ import yaml
 
 # import check_config - TODO: what config checks do we need in this one?
 
-default_config_file = pathlib.Path(__file__).parent / "config" / "pipeline_routine_docker.yaml"
+default_config_file = pathlib.Path(__file__).parent / "config" / "pipeline_routine.yaml"
 
 with open(default_config_file, "r", encoding="utf-8") as default_config:
     WORKFLOW_DEFAULT_CONF = yaml.safe_load(default_config)
