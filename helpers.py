957001b870a79f44832fa9eee6a72f341017762d
Kat Steinke <kat.steinke@rsyd.dk>
2023-10-05 14:47:21 +0200

    helpers: separate translation of sample numbers out into a function

--- helpers.py
+++ helpers.py
@@ -269,6 +269,49 @@ def rearrange_sample_number(old_sample_number: str, pattern_in: re.Pattern,
     return new_sample_number
 
 
+def translate_sample_number(sample_number: str, pattern_in: re.Pattern, pattern_out: re.Pattern,
+                            prefix_mapping: Dict[str, str]) -> str:
+    """Translate a sample number by rearranging it to match a desired output pattern and
+    optionally substituting prefixes.
+
+    Arguments:
+        sample_number:  the original sample number
+        pattern_in:     a pattern describing the original format
+        pattern_out:    a pattern describing the desired format
+        prefix_mapping: a mapping of what prefix to translate to what
+
+    Returns:
+        The rearranged and translated sample number.
+
+    Raises:
+        KeyError:   if the sample number's prefix is not found in the mapping
+        ValueError: if the original sample number does not match the input pattern
+                    or the translated sample number does not match the desired output
+    """
+    original_format_match = re.match(pattern_in, sample_number)
+    if not original_format_match:
+        error_msg = f"Sample number {sample_number} does not match specified input format."
+        raise ValueError(error_msg)
+    start_pattern = re.compile(r"^" + parse_out_group_pattern(pattern_in,
+                                                                      "sample_type").pattern)
+    current_start = original_format_match.group("sample_type")
+    if current_start not in prefix_mapping:
+        error_msg = (f"Prefix {current_start} not found "
+                     f"(allowed prefixes are {list(prefix_mapping.keys())}).")
+        raise KeyError(error_msg)
+    name_translate = re.sub(start_pattern, lambda match: prefix_mapping.get(match.group(),
+                                                                            match.group()),
+                            sample_number)
+    component_order_out = {value: key for key, value in pattern_out.groupindex.items()}
+    name_translate = rearrange_sample_number(name_translate, pattern_in,
+                                                     component_order_out)
+    if not re.match(pattern_out, name_translate):
+        error_msg = (f"Translated sample number {name_translate} (was {sample_number})"
+                     " does not match desired output format.")
+        raise ValueError(error_msg)
+    return name_translate
+
+
 def add_years_in_sheet(runsheet: pd.DataFrame, active_config=workflow_config) -> pd.DataFrame:
     """Add year to sample number from sample year column.
 
@@ -289,7 +332,7 @@ def add_years_in_sheet(runsheet: pd.DataFrame, active_config=workflow_config) ->
     if sample_year_colname not in runsheet.columns:
         raise KeyError("No year column found. "
                        "The pipeline needs a column named 'årstal' to add year to sample number.")
-    # establish what is what
+    # establish what is what # TODO: we can replace the ID pattern thing
     if active_config["sample_number_settings"]["positive_control"]:
         positive_control_pattern = re.compile("|".join(active_config["sample_number_settings"][
                                                 "positive_control"].keys()))
