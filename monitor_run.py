8ab08623fbfa0039a3fcfeff56c1020b6019c336
Kat Steinke <kat.steinke@rsyd.dk>
2024-02-27 11:34:59 +0100

    monitor_run: initialize script for run monitoring

---
+++ monitor_run.py
@@ -0,0 +1,10 @@
+"""Monitor the output directory of a specified sequencing run and start 16S analysis pipeline."""
+
+__author__ = "Kat Steinke"
+
+
+# watch for presence of file
+
+# construct nomad command
+
+# parser to take input? but we need to pass all parameters down
