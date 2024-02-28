664daab46d32500bb55b2dde9f79304445a7b53d
Kat Steinke <kat.steinke@rsyd.dk>
2024-02-28 14:16:09 +0100

    test_start_delay: start a dry run

--- test_pipeline/test_start_delay.sh
+++ test_pipeline/test_start_delay.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir -p run_test/testdir/rawdata/fastq_pass
-python3 /snake_qc/run_pipeline.py --runsheet /data/test_data/16S/test_data/20240124-runsheet-rearrange.xlsx \
+python3 /snake_qc/run_pipeline.py --dry-run \
+        --runsheet /data/test_data/16S/test_data/20240124-runsheet-rearrange.xlsx \
         --rundir run_test \
         --workflow_config_file /snake_qc/config/pipeline_testjob.yml &> delayed_start_log &
 sleep 30
