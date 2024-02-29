ca921e17334a5cc3e566268fe4ba3e67d585dbb3
Kat Steinke <kat.steinke@rsyd.dk>
2024-02-29 08:36:06 +0100

    test: fix directory structure of summary file

--- test_pipeline/test_start_delay.sh
+++ test_pipeline/test_start_delay.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-mkdir -p run_test/testdir/rawdata/fastq_pass
+mkdir -p run_test/rawdata/testdir/fastq_pass
 python3 /snake_qc/run_pipeline.py --dry_run \
         --runsheet /snake_qc/test_pipeline/test_nanopore_runsheet_16s_only.xlsx \
         --rundir run_test \
         --workflow_config_file /snake_qc/test_pipeline/pipeline_testjob_no_lis.yml &> delayed_start_log &
 sleep 30
-touch run_test/testdir/rawdata/final_summary_test.txt
+touch run_test/rawdata/testdir/final_summary_test.txt
 cat delayed_start_log
 grep -e "Found pattern .+ after 30 seconds\." delayed_start_log
