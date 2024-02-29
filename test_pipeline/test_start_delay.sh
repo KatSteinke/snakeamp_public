5df7514c71192562b6628c545f3af01468c2a35f
Kat Steinke <kat.steinke@rsyd.dk>
2024-02-29 08:38:28 +0100

    test: give the pipeline time to find the file and write the log

--- test_pipeline/test_start_delay.sh
+++ test_pipeline/test_start_delay.sh
@@ -7,5 +7,6 @@ python3 /snake_qc/run_pipeline.py --dry_run \
         --workflow_config_file /snake_qc/test_pipeline/pipeline_testjob_no_lis.yml &> delayed_start_log &
 sleep 30
 touch run_test/rawdata/testdir/final_summary_test.txt
+sleep 30
 cat delayed_start_log
 grep -e "Found pattern .+ after 30 seconds\." delayed_start_log
