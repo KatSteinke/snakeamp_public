5b239566f28e93d4d99c7042081bce2a4fb8eb78
Kat Steinke <kat.steinke@rsyd.dk>
2024-03-06 13:53:55 +0100

    run_pipeline: run the waiting in the background

--- test_pipeline/test_start_delay.sh
+++ test_pipeline/test_start_delay.sh
@@ -4,7 +4,7 @@ mkdir -p run_test/rawdata/testdir/fastq_pass
 python3 /snake_qc/run_pipeline.py --dry_run \
         --runsheet /snake_qc/test_pipeline/test_nanopore_runsheet_16s_only.xlsx \
         --rundir run_test \
-        --workflow_config_file /snake_qc/test_pipeline/pipeline_testjob_no_lis.yml &> delayed_start_log &
+        --workflow_config_file /snake_qc/test_pipeline/pipeline_testjob_no_lis.yml &> delayed_start_log
 sleep 30
 touch run_test/rawdata/testdir/final_summary_test.txt
 sleep 30
