8b5dc4e3146801a435f02180c7693da3da4cc976
Kat Steinke <kat.steinke@rsyd.dk>
2026-03-03 10:10:23 +0100

    tests: set up script for Snakemake dry run

---
+++ test_pipeline/test_dry_snakemake.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+mkdir -p run_test/rawdata/testdir/fastq_pass/barcode01
+touch run_test/rawdata/testdir/fastq_pass/barcode01/test.fastq.gz
+mkdir -p run_test/rawdata/testdir/fastq_pass/barcode02
+touch run_test/rawdata/testdir/fastq_pass/barcode02/test.fastq.gz
+mkdir -p run_test/rawdata/testdir/fastq_pass/barcode03
+touch run_test/rawdata/testdir/fastq_pass/barcode03/test.fastq.gz
+mkdir -p run_test/rawdata/testdir/fastq_pass/barcode42
+touch run_test/rawdata/testdir/fastq_pass/barcode42/test.fastq.gz
+touch run_test/rawdata/testdir/final_summary_test.txt
+python3 /snake_qc/run_pipeline.py \
+        --runsheet /snake_qc/test_pipeline/test_nanopore_runsheet_16s_only.xlsx \
+        --rundir run_test/rawdata/testdir/fastq_pass \
+        --workflow_config_file /snake_qc/test_pipeline/pipeline_testjob_no_lis_local.yml \
+         --snake_flags "--dry-run " &> local_log
+# do we need to wait here?
+grep -e "This was a dry-run (flag -n)" local_log
