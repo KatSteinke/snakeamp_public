9c91926931ae6b6000158aa25633a640c3f10ef3
Kat Steinke <kat.steinke@rsyd.dk>
2026-03-04 08:35:34 +0100

    tests: try waiting for Snakemake

--- test_pipeline/test_dry_snakemake.sh
+++ test_pipeline/test_dry_snakemake.sh
@@ -9,12 +9,14 @@ touch run_test/rawdata/testdir/fastq_pass/barcode03/test.fastq.gz
 mkdir -p run_test/rawdata/testdir/fastq_pass/barcode42
 touch run_test/rawdata/testdir/fastq_pass/barcode42/test.fastq.gz
 touch run_test/rawdata/testdir/final_summary_test.txt
+snakemake --version # is snakemake even alive?
 python3 /snake_qc/run_pipeline.py \
         --runsheet /snake_qc/test_pipeline/test_nanopore_runsheet_16s_only.xlsx \
         --rundir run_test/rawdata/testdir/fastq_pass \
         --workflow_config_file /snake_qc/test_pipeline/pipeline_testjob_no_lis_local.yml \
          --snake_flags "--dry-run " > >(tee -a local_log) 2> >(tee -a local_log) # don't save this at all for now so we can see what's happening
 # do we need to wait here?
+sleep 30
 echo "printing local log"
 cat local_log
 grep -e "This was a dry-run (flag -n)" local_log
